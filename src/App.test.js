f14b65a55891ac103e5cc6c189d2f044078c2167
Arun Baskaran <arunmannai@gmail.com>
2024-02-07 13:35:43 +0530

    migrate to vite

--- src/App.test.js
+++
@@ -1,8 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('render title', () => {
-  render(<App />);
-  const title = screen.getByText(/Color Changer/i);
-  expect(title).toBeInTheDocument();
-});
